d093fdf468a6ce288499414544cb9f8412e3cd5a
Frederik <frr001@edu.zealand.dk>
2026-02-10 10:11:33 +0100

    populate person.cs

--- Krypteringsopgave/models/person.cs
+++ Krypteringsopgave/models/person.cs
@@ -1,1 +1,15 @@
+public class Person
+{
+    public int person_id {get; set;}
+    public string first_name {get; set;}
+    public string last_name {get; set;}
+    public string adress {get; set;}
+    public string street_number {get; set;}
+    public string password {get; set;}
+    public bool enabled {get; set;}
 
+    public Person(){
+        
+    }
+
+}
