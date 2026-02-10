a7398c1602e9f47c7a9a3530eb6674fa70dcd328
Sebastian <seb001@edu.zealand.dk>
2026-02-10 12:56:59 +0100

    Lavet første testcase

--- Krypteringsopgaver/Krypteringsopgaver.Test/UnitTests.cs
+++ Krypteringsopgaver/Krypteringsopgaver.Test/UnitTests.cs
@@ -5,13 +5,23 @@ namespace Krypteringsopgaver.Test;
 public class UnitTests
 {
     [Fact]
-    public void Test1()
+    public async Task GetById_Pass()
     {
+        // Given
         var dpPath = Path.Combine(AppContext.BaseDirectory, "data", "flat_file_db.json");
         PersonRepository repo = new PersonRepository(dpPath);
         
-        var persons = repo.GetAllAsync();
+        // When
+        var person = await repo.GetById(1);
         
-        Assert.True(persons.Result.Count > 0);
+        // Then
+        Assert.Equal(1, person.Person_id);
+        Assert.Equal("John", person.First_name);
+        Assert.Equal("Doe", person.Last_name);
+        Assert.Equal("Somewhere", person.Adress);
+        Assert.Equal("", person.Street_number);
+        Assert.Equal("Test", person.Password);
+        Assert.True(person.Enabled);
+        
     }
 }
